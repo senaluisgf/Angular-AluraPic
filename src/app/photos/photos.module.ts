e7993e8f6c38cd3ed879eae0efe7356ebfbe843b
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-01-18 14:07:38 -0400

    Adicionando rotas na aplicação

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common';
+
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    declarations: [ 
+        PhotoComponent, 
+        PhotoListComponent, 
+        PhotoFormComponent 
+    ],
+    exports: [ ],
+    imports: [ 
+        HttpClientModule,
+        CommonModule
+    ]
 })
 export class PhotosModule {}
