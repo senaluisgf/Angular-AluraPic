ae1f70d8ea45ca6655e68c80c2c49b19a0c1119a
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-01-19 08:44:58 -0400

    adicionando modulo de erros na aplicação

---
+++ src/app/errors/errors.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+    declarations: [ NotFoundComponent ],
+    exports: [],
+    imports: [ CommonModule ]
+})
+export class ErrorsModule {}
