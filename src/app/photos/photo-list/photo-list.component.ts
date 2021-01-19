af9510d0a7ef735bf6db2522922c8dce7a91f09e
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-01-19 14:29:08 -0400

    melhorando a filtragem dentro da aplicação

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
 
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = []
   filter: string = ''
+  debounce: Subject<string> = new Subject<string>();
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
 
-  ngOnInit() {
-    const userName = this.activatedRoute.snapshot.params.userName
-    this.photoService
-      .listeFromUser(userName)
-      .subscribe(photos => this.photos = photos)
+  constructor(private activatedRoute: ActivatedRoute) {}
+  
+  ngOnInit(): void {
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter=filter)
+    this.photos = this.activatedRoute.snapshot.data.photos
   }
-
+  
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
