d407887dc9137e073612fe15578a277675510036
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-01-20 14:00:32 -0400

    componetizando a busca e separando imagens em cards

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators'
+
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -11,10 +10,9 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
   photos: Photo[] = []
   filter: string = ''
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true
   userName: string = ''
   currentPage: number = 1
@@ -28,21 +26,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params.userName
     this.photos = this.activatedRoute.snapshot.data.photos
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter=filter)
   }
 
   load(){
     this.photoService
       .listeFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
+        this.filter = ''
         this.photos = this.photos.concat(photos);
         if(!photos.length) this.hasMore = false
       })
   }
   
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
-  }
 }
--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -2,23 +2,27 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import { PhotoModule } from "../photo/photo.module";
+import { CardModule } from "src/app/shared/components/card/card.module";
 
 import { PhotoListComponent } from "./photo-list.component";
 import { PhotosComponent } from "./photos/photos.component";
 import { LoadButtonComponent } from "./load-button/load-button.component";
 
 import { FilterByDescriptionPipe } from "./filter-by-description.pipe";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
     declarations:[
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescriptionPipe
+        FilterByDescriptionPipe,
+        SearchComponent
     ],
     imports:[
         CommonModule,
-        PhotoModule
+        PhotoModule,
+        CardModule
     ]
 })
 export class PhotoListModule {}
