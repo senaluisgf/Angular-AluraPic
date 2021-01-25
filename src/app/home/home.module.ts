867bef424e6a1156774bc0527e321b74749ad8af
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-01-25 13:10:30 -0400

    Redirecionando da página de login para a página do usuário

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core'
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms'
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports:[
         CommonModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        RouterModule
     ],
     declarations: [SigninComponent]
 })
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
 
 import { AuthService } from 'src/app/core/auth.service'
 
@@ -11,7 +12,8 @@ export class SigninComponent implements OnInit{
 
     constructor(
         private formBuilder: FormBuilder,
-        private authService: AuthService
+        private authService: AuthService,
+        private router: Router
     ){}
 
     ngOnInit(): void{
@@ -28,7 +30,7 @@ export class SigninComponent implements OnInit{
         this.authService
             .authenticate(userName, password)
             .subscribe(
-                () => console.log('autenticado'),
+                () => this.router.navigate(['user', userName]),
                 err => {
                     console.log(err)
                     alert('Invalid user name or password')
