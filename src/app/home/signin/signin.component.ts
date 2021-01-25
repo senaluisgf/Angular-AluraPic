dd9e0cd6cb4f32d89966e82740dc3e1465392bbb
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-01-25 13:39:14 -0400

    Autofocus quando login esta incorreto

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 
-import { AuthService } from 'src/app/core/auth.service'
+import { AuthService } from 'src/app/core/auth/auth.service'
 
 @Component({
     templateUrl: './signin.component.html'
 })
 export class SigninComponent implements OnInit{
+
     loginForm: FormGroup
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>
 
     constructor(
         private formBuilder: FormBuilder,
@@ -32,7 +34,7 @@ export class SigninComponent implements OnInit{
             .subscribe(
                 () => this.router.navigate(['user', userName]),
                 err => {
-                    console.log(err)
+                    this.userNameInput.nativeElement.focus()
                     alert('Invalid user name or password')
                     this.loginForm.reset()
                 }
