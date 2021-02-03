9fb49bd00ab8bedc551c258c18023d321e0fcd0f
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-02-03 10:55:40 -0400

    Adicionando Validador assincrono (userNameIsTaken)

---
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { AbstractControl } from "@angular/forms";
+import { debounceTime, first, map, switchMap } from "rxjs/operators";
+import { SignUpService } from "./signup.service";
+
+@Injectable({ providedIn: 'root' })
+export class UserNotTakenValidatorService {
+
+    constructor(private signUpService: SignUpService){}
+
+    checkUserNameTaken(){
+        return (control: AbstractControl) => {
+            return control
+                .valueChanges
+                .pipe(debounceTime(300))
+                .pipe(switchMap(userName => {
+                    return this.signUpService.checkUserNameTaken(userName)
+                }))
+                .pipe(map(isTaken => isTaken ? { userNameIsTaken: true } : null))
+                .pipe(first())
+        }
+    }
+}
