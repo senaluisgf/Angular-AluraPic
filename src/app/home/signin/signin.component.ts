bcedcd87f8f92b4d3929f9f51708972ac30c6d86
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-03-01 08:28:56 -0400

    Alterando título das páginas

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Title } from '@angular/platform-browser'
 import { Router } from '@angular/router'
 
 import { AuthService } from 'src/app/core/auth/auth.service'
@@ -18,10 +19,12 @@ export class SignInComponent implements OnInit{
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService
+        private platformDetectorService: PlatformDetectorService,
+        private titleService: Title
     ){}
 
     ngOnInit(): void{
+        this.titleService.setTitle('Login')
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
