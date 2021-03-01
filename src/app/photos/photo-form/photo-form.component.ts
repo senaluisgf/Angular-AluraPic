832e407f231ba4e71db43e5cad67fe94f3d3cf06
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-03-01 10:38:24 -0400

    Exibindo status de progresso para upload de foto

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,8 @@
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
@@ -15,6 +17,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,10 +40,18 @@ export class PhotoFormComponent implements OnInit {
     const allowComnets = this.photoForm.get('allowComments').value
     this.photoService
       .upload(description, allowComnets, this.file)
+      .pipe(finalize(() => this.router.navigate(['/user', this.userService.getUserName()])))
       .subscribe(
-        () => {
+        (event: HttpEvent<any>) => {
+          if(event.type == HttpEventType.UploadProgress){
+            this.percentDone = Math.round(100 * event.loaded/event.total)
+          } else if(event instanceof HttpResponse){
             this.alertService.success('Photo upload successfly', true)
-            this.router.navigate(['/user', this.userService.getUserName()])
+          }
+        },
+        err => {
+          console.log(err)
+          this.alertService.danger("Upload Error!!!", true)
         }
       )
   }
